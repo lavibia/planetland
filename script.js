7b6f1ead35053a864fbc56653ae3374166cf2389
Lavinia Bianca <chelaru_laviniabianca@yahoo.com>
2024-03-10 23:44:47 +0100

    set active button

--- script.js
+++ script.js
@@ -1,133 +1,183 @@
 let btnOverview = document.getElementById("btnOverview");
 let btnStructure = document.getElementById("btnStructure");
 let btnSurface = document.getElementById("btnSurface");
+let imgOverview = document.getElementById("img-overview");
+let imgStructure = document.getElementById("img-internal");
+let imgSurface = document.getElementById("img-geology");
+let txtOverview = document.getElementById("overview-holder");
+let txtStructure = document.getElementById("structure-holder");
+let txtSurface = document.getElementById("geology-holder");
+
+
 let hamburgerIcon = document.querySelector('.hamburger');
 let phoneNav = document.querySelector(".phone-nav");
 let content = document.querySelector(".content");
 let desktopNavLinks = document.getElementsByClassName('nav')[0].children;
 console.log(desktopNavLinks);
+let planet = document.getElementById("title").textContent.toLowerCase();
+const phoneMedia = window.matchMedia("(max-width: 767px)");
+
+
+activateButton('overview');
 
 
 //Planet color on nav item hover
 for (let i = 0; i < desktopNavLinks.length; i++) {
-      desktopNavLinks[i].setAttribute('onmouseenter', 'displayHoverNavColor(event.target)')
-      desktopNavLinks[i].setAttribute('onmouseleave', 'displayHoverNavColor(event.target)')
+  desktopNavLinks[i].setAttribute('onmouseenter', 'displayHoverNavColor(event.target)')
+  desktopNavLinks[i].setAttribute('onmouseleave', 'displayHoverNavColor(event.target)')
 }
 
 function displayHoverNavColor() {
-      if (event.type == "mouseenter") {
-            //get the link planet to set the color
-            let link = event.target.children[0].getAttribute('href');
-            let planet = link.slice(2, link.lastIndexOf('.'))
-            if (planet == 'index')
-                  planet = 'mercury'
+  if (event.type == "mouseenter") {
+    //get the link planet to set the color
+    let link = event.target.children[0].getAttribute('href');
+    let planet = link.slice(2, link.lastIndexOf('.'))
+    if (planet == 'index')
+      planet = 'mercury'
 
-            event.target.style.borderTop = `2px solid var(--color-${planet}`;
-      } else {
-            event.target.style.borderTop = '0px';
-      }
+    event.target.style.borderTop = `2px solid var(--color-${planet}`;
+  } else {
+    event.target.style.borderTop = '0px';
+  }
 }
 
 
 //TOGGLE PHONE NAVIGATION
 hamburgerIcon.addEventListener("click", () => {
-      if (phoneNav.style.display == "flex") {
-            hamburgerIcon.style.filter = 'none';
-            phoneNav.style.display = "none"
-      } else {
-            phoneNav.style.display = "flex"
-            phoneNav.style.left = 0;
-            hamburgerIcon.style.filter = 'invert(21%) sepia(11%) saturate(1402%) hue-rotate(201deg) brightness(92%) contrast(89%)';
-      }
+  if (phoneNav.style.display == "flex") {
+    hamburgerIcon.style.filter = 'none';
+    phoneNav.style.display = "none"
+  } else {
+    phoneNav.style.display = "flex"
+    phoneNav.style.left = 0;
+    hamburgerIcon.style.filter = 'invert(21%) sepia(11%) saturate(1402%) hue-rotate(201deg) brightness(92%) contrast(89%)';
+  }
 })
+
+
 
 //change buttons text on phone view
-const phoneMedia=window.matchMedia("(max-width: 767px)");
-phoneMedia.addEventListener("change", function(){
-      if (phoneMedia.matches) {
-            btnOverview.innerHTML='OVERVIEW';
-            btnStructure.innerHTML='STRUCTURE';
-            btnSurface.innerHTML='SURFACE';
-       }
-       else{
-             btnOverview.innerHTML='<span class="btn-text-number">01</span>OVERVIEW';
-             btnStructure.innerHTML='<span class="btn-text-number">02</span>INTERNAL STRUCTURE';
-             btnSurface.innerHTML='<span class="btn-text-number">03</span>SURFACE GEOLOGY';
-       } 
 
+phoneMedia.addEventListener("change", function () {
+  if (phoneMedia.matches) {
+    btnOverview.innerHTML = 'OVERVIEW';
+    btnStructure.innerHTML = 'STRUCTURE';
+    btnSurface.innerHTML = 'SURFACE';
+  }
+  else {
+    btnOverview.innerHTML = '<span class="btn-text-number">01</span>OVERVIEW';
+    btnStructure.innerHTML = '<span class="btn-text-number">02</span>INTERNAL STRUCTURE';
+    btnSurface.innerHTML = '<span class="btn-text-number">03</span>SURFACE GEOLOGY';
+  }
+
+})
+phoneMedia.addEventListener("change", function () {
+  Array(btnOverview, btnStructure, btnSurface).forEach(btn => {
+    if (btn.classList.contains('active')) {
+      console.log(btn)
+      activateButton(btn.id.slice(3).toLowerCase());
+    }
+  });
+})
+
+//set active content
+/* Click on button
+- Change color button
+- Remove color other button
+- Display content matching button
+- Hide content unmatching button
+- Display image relevant
+- Hide image irrelevant */
+
+btnOverview.addEventListener('click', () => {
+  activateButton('overview');
+  showContent('overview');
+  showImage('overview');
+})
+btnStructure.addEventListener('click', () => {
+  activateButton('structure');
+  showContent('structure');
+  showImage('structure');
+})
+btnSurface.addEventListener('click', () => {
+  activateButton('surface');
+  showContent('surface');
+  showImage('surface');
 })
 
 
+//change style on active buttons
 
-// const navLink = document.querySelectorAll(".nav-link");
+function activateButton(topic) {
+  if (phoneMedia.matches) {
+    if (topic === "overview") {
+      btnOverview.classList.remove(`bg-${planet}`)
+      btnOverview.classList.add('active');
+      btnOverview.style.borderBottom = `3px solid var(--color-${planet})`
+      btnStructure.style.borderBottom = '1px solid var(--color-light)';
+      btnSurface.style.borderBottom = '1px solid var(--color-light)';
+    } else if (topic === "structure") {
+      btnStructure.classList.remove(`bg-${planet}`)
+      btnStructure.classList.add('active');
+      btnStructure.style.borderBottom = `3px solid var(--color-${planet})`
+      btnOverview.style.borderBottom = '1px solid var(--color-light)';
+      btnSurface.style.borderBottom = '1px solid var(--color-light)';
+    } else {
+      btnSurface.classList.remove(`bg-${planet}`)
+      btnSurface.classList.add('active');
+      btnSurface.style.borderBottom = `3px solid var(--color-${planet})`
+      btnStructure.style.borderBottom = '1px solid var(--color-light)';
+      btnOverview.style.borderBottom = '1px solid var(--color-light)';
+    }
+  }
+  else {
+    if (topic === "overview") {
+      btnOverview.style.borderBottom = '1px solid var(--color-light)';
+      btnOverview.classList.add(`bg-${planet}`, 'active');
+      btnStructure.classList.remove(`bg-${planet}`, 'active');
+      btnSurface.classList.remove(`bg-${planet}`, 'active');
+    } else if (topic === "structure") {
+      btnStructure.style.borderBottom = '1px solid var(--color-light)';
+      btnOverview.classList.remove(`bg-${planet}`, 'active');
+      btnStructure.classList.add(`bg-${planet}`, 'active');
+      btnSurface.classList.remove(`bg-${planet}`, 'active');
+    } else {
+      btnSurface.style.borderBottom = '1px solid var(--color-light)';
+      btnOverview.classList.remove(`bg-${planet}`, 'active');
+      btnStructure.classList.remove(`bg-${planet}`, 'active');
+      btnSurface.classList.add(`bg-${planet}`, 'active');
+    }
+  }
+}
 
-// navLink.forEach(n => n.addEventListener("click", closeMenu));
-
-// function closeMenu() {
-//     hamburger.classList.remove("active");
-//     navMenu.classList.remove("active");
-// }
-
-
-let btnContent = document.querySelector(".btn-content");
-
-let planet = document.getElementById("title").textContent.toLowerCase();
-
-console.log(planet);
-//btnContent[i].classList.add
-console.log(`active-${planet}`);
-btnContent.style.borderBottom = `3px solid var(--color-${planet})`
-console.log(btnContent);
-
-// for(let i=0;i<btnContent.length;i++){
-//     btnContent[i].addEventListener("onclick",()=>{
-console.log(planet);
-//btnContent[i].classList.add
-console.log(`active-${planet}`);
-//     })
-// }
-//
-
-
-
-/* ideas for the set active button
-
-
-look for when a button is clicked
-
-functon set active button
--add bg-planet clas to the button clicked, remove bg-planet for the other button
-
-function display image
-  if the first button is clicked
-  IMAGE
-        display the first image
-        hide the second and third image
-  CONTENT
-        display the first div content
-        hide the second and third div
-  if the second button is clicked
-  IMAGE
-        display the second image
-        hide the first and third image
-  CONTENT
-     display the second div content
-     hide the first and third div
-  if the third button is clicked
-  IMAGE
-        display the first and third image
-        hide the second iamge
-  CONTENT
-        display the third div content
-     hide the first and the second div
-
-     keep buttons and content and images in arrays for easier acces and 
-
-
-     leaga cumva contentul care trebuie afisat
-     imaginile si textul in functie de categorie
-     display(categorie)
-
-     functie if(categorie display)
-     else hide
-*/
+function showImage(topic) {
+  if (topic === "overview") {
+    imgOverview.classList.remove('hidden');
+    // imgOverview.classList.add('show');
+    imgStructure.classList.add('hidden');
+    imgSurface.classList.add('hidden');
+  } else if (topic === "structure") {
+    imgOverview.classList.add('hidden');
+    imgStructure.classList.remove('hidden');
+    imgSurface.classList.add('hidden');
+  } else {
+    imgOverview.classList.remove('hidden');
+    imgStructure.classList.add('hidden');
+    imgSurface.classList.remove('hidden');
+  }
+}
+function showContent(topic) {
+  if (topic === 'overview') {
+    txtOverview.classList.remove('hidden');
+    txtStructure.classList.add('hidden');
+    txtSurface.classList.add('hidden');
+  } else if (topic === 'structure') {
+    txtOverview.classList.add('hidden');
+    txtStructure.classList.remove('hidden');
+    txtSurface.classList.add('hidden');
+  } else {
+    txtOverview.classList.add('hidden');
+    txtStructure.classList.add('hidden');
+    txtSurface.classList.remove('hidden');
+  }
+}
